6834b6c61f1ddbd3791f7622c1f915b3d2345efc
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-02-12 21:13:28 -0500

    Constructor added

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,1 +1,15 @@
+public class KnightBoard {
+  private int[][] board;
 
+  public KnightBoard (int startingRows, int startingCols) {
+    if (startingRows < 0 || startingCols < 0) {
+      throw new IllegalArgumentException("Dimensions cannot be negative");
+    }
+    board = new int[row][col];
+    for (int r = 0; r < startingRows; r++) {
+      for (int c = 0; c < startingCols; c++) {
+        board[r][c]=0;
+      }
+    }
+  }
+}
