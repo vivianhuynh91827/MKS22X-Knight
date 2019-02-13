e914654117234339d8219f9ec327d73c34a3c7d0
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-02-13 10:54:37 -0500

    toString completed

--- KnightBoard.java
+++ KnightBoard.java
@@ -2,14 +2,45 @@ public class KnightBoard {
   private int[][] board;
 
   public KnightBoard (int startingRows, int startingCols) {
-    if (startingRows < 0 || startingCols < 0) {
+    if (startingRows <= 0 || startingCols <= 0) {
       throw new IllegalArgumentException("Dimensions cannot be negative");
     }
-    board = new int[row][col];
+    board = new int[startingRows][startingCols];
     for (int r = 0; r < startingRows; r++) {
       for (int c = 0; c < startingCols; c++) {
         board[r][c]=0;
       }
     }
+  }
+
+  public String toString() {
+    String boardString = "";
+    boolean doubDig = board.length * board[0].length > 9;
+    for (int r = 0; r < board.length; r ++) {
+      for (int c = 0; c < board[0].length; c ++) {
+        if (board[r][c]==0) {
+          if (doubDig) boardString += " _ ";
+          else boardString += "_ ";
+        }
+        else {
+          if (doubDig) {
+            if (board[r][c]<10) boardString += " " + board[r][c] + " ";
+            else boardString+= board[r][c] + " ";
+          }
+          else {
+            boardString+=board[r][c]+" ";
+          }
+        }
+      }
+      boardString += "\n";
+    }
+    return boardString.substring(0,boardString.length()-1);
+  }
+  
+  public static void main(String[] args) {
+    KnightBoard test = new KnightBoard(2,2);
+    System.out.println(test);
+    KnightBoard test2 = new KnightBoard(4,6);
+    System.out.println(test2);
   }
 }
