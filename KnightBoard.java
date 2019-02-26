84b666cf4d15497bb0401f9edb39aa4b40142cc3
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-02-26 18:36:03 -0500

    count method completed

--- KnightBoard.java
+++ KnightBoard.java
@@ -60,34 +60,70 @@ public class KnightBoard {
     //if the starting col is out of range return false;
     if (board[startingR][startingC] != 0) return false;
     //if the current space is not empty return false;
-    // board[startingR][startingC]=level;
     int[][] increments = {{1,1,-1,-1,2,2,-2,-2}, {2,-2,2,-2,1,-1,1,-1}};
     for (int i = 0; i < 8; i++) {
       int newR = startingR + increments[0][i];
       int newC = startingC + increments[1][i];
       board[startingR][startingC] = level;
       if (solveH(newR, newC, level+1)) return true;
-      // else if (newR > -1 && newC > -1 && newR < board.length && newC < board[0].length) {
-      //   board[newR][newC] = 0;}
       board[startingR][startingC] = 0;
     }
     return false;
-    // return solveH(startingR+2, startingC+1,level+1) ||
-    //        solveH(startingR+2, startingC-1,level+1) ||
-    //        solveH(startingR-2, startingC+1,level+1) ||
-    //        solveH(startingR-2, startingC-1,level+1) ||
-    //        solveH(startingR+1, startingC+2,level+1) ||
-    //        solveH(startingR+1, startingC-2,level+1) ||
-    //        solveH(startingR-1, startingC+2,level+1) ||
-    //        solveH(startingR-1, startingC-2,level+1);
   }
+
+  public int countSolutions(int startingRow, int startingCol) {
+    if (startingRow < 0 || startingCol < 0) {
+      throw new IllegalArgumentException();
+    }
+    for (int r = 0; r < board.length; r++) {
+      for (int c = 0; c < board[0].length; c++) {
+        if (board[r][c] != 0) {
+          throw new IllegalStateException();
+        }
+      }
+    }
+    return countH(startingRow, startingCol, 1);
+  }
+
+  private int countH(int startingR, int startingC, int level) {
+    int count = 0;
+    int[][] increments = {{1,1,-1,-1,2,2,-2,-2}, {2,-2,2,-2,1,-1,1,-1}};
+    if (addKnight(startingR, startingC, level)) {
+      if (level == board.length*board[0].length) {
+        removeKnight(startingR, startingC);
+        return 1;
+      }
+      else {
+        for (int i = 0; i < 8; i++) {
+          count += countH(startingR + increments[0][i], startingC + increments[1][i], level+1);
+        }
+      }
+      removeKnight(startingR, startingC);
+    }
+    return count;
+  }
+
+  private boolean addKnight(int row, int col, int level) {
+    if (row >= board.length || row < 0) return false;
+    if (col >= board[0].length || col < 0) return false;
+    if (board[row][col] != 0) return false;
+    board[row][col]=level;
+    return true;
+  }
+
+  private void removeKnight(int row, int col) {
+    board[row][col] = 0;
+  }
+
 
   public static void main(String[] args) {
     // KnightBoard test = new KnightBoard(4,4);
     // System.out.println(test);
-    KnightBoard test2 = new KnightBoard(3,7);
+     KnightBoard test2 = new KnightBoard(3,3);
+     System.out.println(test2);
+     System.out.println(test2.countSolutions(0,0));
+    // System.out.println(test2.solve(0,0));
     // System.out.println(test2);
-    System.out.println(test2.solve(0,0));
-    System.out.println(test2);
+
   }
 }
